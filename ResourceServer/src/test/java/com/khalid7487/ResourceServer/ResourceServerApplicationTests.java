07effe59b29ca6d8d7e25cbe72be8c5d10278bd1
khalid7487 <khalid15-7487@diu.edu.bd>
2021-06-17 12:18:33 +0600

    init resource server

---
+++ ResourceServer/src/test/java/com/khalid7487/ResourceServer/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalid7487.ResourceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
