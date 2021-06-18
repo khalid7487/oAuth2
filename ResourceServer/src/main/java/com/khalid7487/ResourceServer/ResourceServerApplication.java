7283a160483f5d509d845bdcd78d987632a05eac
khalid7487 <khalid15-7487@diu.edu.bd>
2021-06-19 00:56:58 +0600

    config resouce server as a eureka server

--- ResourceServer/src/main/java/com/khalid7487/ResourceServer/ResourceServerApplication.java
+++ ResourceServer/src/main/java/com/khalid7487/ResourceServer/ResourceServerApplication.java
@@ -2,7 +2,9 @@ package com.khalid7487.ResourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResourceServerApplication {
 
