324b0eaeed5c32f2f3558d1006fd90e6901b0f4a
khalid7487 <khalid15-7487@diu.edu.bd>
2021-06-18 12:53:18 +0600

    method level security

--- ResourceServer/src/main/java/com/khalid7487/ResourceServer/controllers/UsersController.java
+++ ResourceServer/src/main/java/com/khalid7487/ResourceServer/controllers/UsersController.java
@@ -1,9 +1,13 @@
 package com.khalid7487.ResourceServer.controllers;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.khalid7487.ResourceServer.response.UserRest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -12,5 +16,19 @@ public class UsersController {
     @GetMapping("/status/check")
     public  String status(){
         return "working ......";
+    }
+
+    //@Secured("ROLE_developer")
+    @PreAuthorize("hasRole('developer') or #id == #jwt.subject")
+    @DeleteMapping(path = "/{id}")
+    public String delete(@PathVariable String id , @AuthenticationPrincipal Jwt jwt){
+
+        return "Delete user with id " + id + " and JWT subject "+ jwt.getSubject();
+    }
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return new UserRest("khalid", "Hossen","6eefa5b8-4423-4c0f-b2ba-897309550424");
     }
 }
--- ResourceServer/src/main/java/com/khalid7487/ResourceServer/security/Websecurity.java
+++ ResourceServer/src/main/java/com/khalid7487/ResourceServer/security/Websecurity.java
@@ -1,12 +1,17 @@
 package com.khalid7487.ResourceServer.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true,
+                            prePostEnabled = true)
 @EnableWebSecurity
 public class Websecurity extends WebSecurityConfigurerAdapter {
 
